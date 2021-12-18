179cef28d99a7815e1457838edb5bcac390edcc6
diivanov <diivanov@itmo.ru>
2021-12-19 01:38:48 +0300

    Updates

--- main.b37ab04b2c7134349103.js
+++
@@ -1,2 +0,0 @@
-(()=>{"use strict";(()=>{const t=document.getElementById("cart"),c=document.querySelector(".cart");t.addEventListener("click",(()=>{c.classList.add("active")})),c.addEventListener("click",(t=>{const e=t.target;(e.classList.contains("cart")||e.classList.contains("cart-close"))&&c.classList.remove("active")}))})()})();
-//# sourceMappingURL=main.b37ab04b2c7134349103.js.map
